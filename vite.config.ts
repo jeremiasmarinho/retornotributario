2e66d28e8076d344159a1158af73b1c73305ce7b
jeremiasmarinho <jeremias10@msn.com>
2025-11-19 18:45:20 -0300

    fix: simplify vercel build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,16 @@ import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
 import { defineConfig } from "vite";
 
-const isVercel = process.env.VERCEL !== undefined;
-
 export default defineConfig({
   plugins: [
-    isVercel ? null : build(),
+    build(),
     devServer({
       adapter,
       entry: "src/index.tsx",
     }),
-  ].filter(Boolean),
+  ],
   build: {
     minify: "terser",
     sourcemap: false,
-    rollupOptions: isVercel
-      ? {
-          input: "src/index.tsx",
-          output: {
-            dir: "dist",
-            format: "esm",
-          },
-        }
-      : undefined,
   },
 });
